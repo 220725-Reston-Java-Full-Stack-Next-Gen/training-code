bea49a21918422bf6475f44e96871143ffa5f597
Tiffany Obi <tiffanyobi123@gmail.com>
2022-08-09 16:58:00 -0400

    finished ConnectionUtil

---
+++ demos/2-Core Language Fundamentals/HelloJDBC/src/main/java/com/revature/drivers/Main.java
@@ -0,0 +1,9 @@
+package com.revature.drivers;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(System.getenv("db_url"));
+		
+	}
+}
--- demos/2-Core Language Fundamentals/HelloJDBC/src/main/java/com/revature/util/JDBCConnectionUtil.java
+++ demos/2-Core Language Fundamentals/HelloJDBC/src/main/java/com/revature/util/JDBCConnectionUtil.java
@@ -1,6 +1,8 @@
 package com.revature.util;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JDBCConnectionUtil {
 	//this utility class will establish a database connection between this Java program
@@ -18,6 +20,33 @@ public class JDBCConnectionUtil {
 		//PASSWORD: same as above
 		
 		//2. establish our Connection's Driver Manager to make a new connection based on provided creds
+		Connection conn = null;
+		
+		
+		try {
+			
+			// here we will use our environment variables to 
+			System.out.println(String.format("Making a database connection with the following credebtials: \nURL: %s \nUsername: %s \nPassword: %s",
+					System.getenv("db_url"),
+					System.getenv("db_username"),
+					System.getenv("db-password")));
+			
+			conn = DriverManager.getConnection(
+					System.getenv("db_url"),
+					System.getenv("db-username"),
+					System.getenv("db_password")
+					
+					);
+			
+			
+		} catch (SQLException e) {
+			
+			System.out.println(e.getMessage());
+			
+		}
+		
+		return conn;
+		
 	}
 
 }
