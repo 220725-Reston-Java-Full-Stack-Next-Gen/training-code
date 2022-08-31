b5c7c29a4a152117e98a8fe174a9481a068c05fe
aaknox <azhya.knox@gmail.com>
2022-08-31 09:17:08 -0500

    finished with array.js example

--- demos/6-Web Development/javascript/3-arrays/script.js
+++ demos/6-Web Development/javascript/3-arrays/script.js
@@ -12,14 +12,39 @@ parentDiv.append(elem);
 let line = document.createElement("hr");
 parentDiv.append(line);
 //iterating through our array can be achieved in many different ways
+let header = document.createElement("h3");
+header.innerHTML = "Full DBZ Character List";
+parentDiv.append(header);
 
 //1. using a for loop
 let container = document.createElement("div");
-for(let i = 0; i < dbzCharacters.length; i++){
-    const character = dbzCharacters[i];
-    var placeholder = document.createElement("p");
-    placeholder.innerHTML = character;
-    container.append(placeholder);
-}
+
+// for(let i = 0; i < dbzCharacters.length; i++){
+//     const character = dbzCharacters[i];
+//     var placeholder = document.createElement("p");
+//     placeholder.innerHTML = character;
+//     container.append(placeholder);
+// }
+
+//2. using the forEach function = uses an anonymous function (aka arrow function) to execute code
+// dbzCharacters.forEach((element) => {
+//     //this is where I can do some logic on each element within the array
+//     let placeholder = document.createElement("span");
+//     //no need for access to the index number when using the foreach function
+//     placeholder.innerHTML = `${element} <br>`;
+//     container.append(placeholder);
+// });
+
+//3. using the for-in/for-of loop
+//this approach is preferred for collections or objects in JS
+for(const character in dbzCharacters){
+    //here I am using the object prototype to call on each element within the array
+    if(Object.hasOwnProperty.call(dbzCharacters, character)){
+        const person = dbzCharacters[character];
+        let placeholder = document.createElement("h3");
+        placeholder.innerHTML = `${person} <br>`;
+        container.append(placeholder);
+    }
+} 
 
 parentDiv.append(container);
