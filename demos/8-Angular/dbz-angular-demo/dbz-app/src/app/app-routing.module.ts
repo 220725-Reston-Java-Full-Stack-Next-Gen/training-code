64217390e453c498e36eddc3f5f8946c694a397b
aaknox <azhya.knox@gmail.com>
2022-09-13 15:24:16 -0500

    added routing to app

--- demos/8-Angular/dbz-angular-demo/dbz-app/src/app/app-routing.module.ts
+++ demos/8-Angular/dbz-angular-demo/dbz-app/src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+//here is where we would define our routes
+const routes: Routes = [
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  },
+  //here is where you can define a default path for when the user either doesn't provide a valid path or no path at all
+  {
+    path: "",
+    redirectTo: '/login',
+    pathMatch: 'full' //this property will check if there's a full match to the provided pattern in the URL and handle the page redirect from there using our desired redirection endpoint
+    //ex. http://localhost:4200 -> auto sent to the login page
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
+//This module's responsibility to manage and provide navigation routes for our Angular app
 export class AppRoutingModule { }
