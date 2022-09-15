81476fda5da2742f82a2d2ae9de05ddb2d4f836e
aaknox <azhya.knox@gmail.com>
2022-09-15 16:10:31 -0500

    added view all component code for observables demo

--- demos/8-Angular/dbz-angular-demo/dbz-app/src/app/components/blogs/view-all/view-all.component.ts
+++ demos/8-Angular/dbz-angular-demo/dbz-app/src/app/components/blogs/view-all/view-all.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Blog } from 'src/app/models/blog';
+import { BlogService } from 'src/app/services/blog.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-view-all',
@@ -6,10 +11,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-all.component.css']
 })
 export class ViewAllComponent implements OnInit {
+  blogs!: Blog[];
+  newBlogs!: Blog[];
+  //this is how you set a localVariable as an observable 
+  public blog!: Observable<Blog>;
+  activeId!: number;
 
-  constructor() { }
+  constructor(private changeDetect: ChangeDetectorRef, private service: BlogService, private userService: UserService, private router: Router) { }
 
   ngOnInit(): void {
+    console.log(localStorage.getItem("current-user") || "");
+    this.service.getCurrentUser();
+    this.getAllBlogs();
+  }
+  getAllBlogs() {
+    //1. subscribe to the observable that returns from the service call
+    this.service.getAllBlogs().subscribe(
+      data => {
+        // print result data
+        console.log(`Backend data: ${data}`);
+
+        //set the blogs array to the result data
+        this.blogs = data;
+        console.log("Blogs: " + this.blogs);
+
+        //check for detectable changes in the HTML template
+        this.changeDetect.detectChanges();
+      }
+    );
+
+    //2. set the blogs array to equal the newBlogs array for proper reset for changeDetect
+    this.blogs = this.newBlogs;
   }
 
 }
