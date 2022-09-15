392b165d9d4fe3a46e9c257a106c7fe7a09379a6
aaknox <azhya.knox@gmail.com>
2022-09-15 14:29:26 -0500

    added full routing for angular demo

--- demos/8-Angular/dbz-angular-demo/dbz-app/src/app/app-routing.module.ts
+++ demos/8-Angular/dbz-angular-demo/dbz-app/src/app/app-routing.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CreateComponent } from './components/blogs/create/create.component';
+import { ViewAllComponent } from './components/blogs/view-all/view-all.component';
+import { BrowseComponent } from './components/browse/browse.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -17,6 +21,30 @@ const routes: Routes = [
   {
     path: "home",
     component: HomeComponent
+  },
+  //view all blogs endpoint to go to the ViewAllComponent
+  {
+    path: "blogs/view-all",
+    component: ViewAllComponent
+  },
+  //create new blog enpoint to go to the CreateComponent
+  {
+    path: "blogs/create",
+    component: CreateComponent
+  },
+  {
+    path: "about",
+    component: AboutComponent
+  },
+  {
+    path: "browse",
+    component: BrowseComponent
+  },
+  //for the browse endpoint, I also want the user to be able to extract their results from URL pattern when browse
+  {
+    //route params come in play here - you can define a param by using :param-name at any point of your path property
+    path: "browse/:searchTerm",
+    component: BrowseComponent
   },
   //here is where you can define a default path for when the user either doesn't provide a valid path or no path at all
   {
