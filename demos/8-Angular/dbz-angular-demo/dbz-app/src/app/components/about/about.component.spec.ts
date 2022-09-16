e358b39af65e17f52b0b2722f48e9a9b606f7dfb
aaknox <azhya.knox@gmail.com>
2022-09-16 10:50:04 -0500

    angular testing on about component - karma now works when configured to Firefox instead of Chrome

--- demos/8-Angular/dbz-angular-demo/dbz-app/src/app/components/about/about.component.spec.ts
+++ demos/8-Angular/dbz-angular-demo/dbz-app/src/app/components/about/about.component.spec.ts
@@ -1,7 +1,25 @@
+/*
+  Topic: Angular Testing
+
+  - Angular provides a testing framework within its initial dependencies that help us perform behavior-driven test cases for any given component/service/module within the project.
+
+  - What is BDD (behavior driven testing)?
+    = a way for software development teams to test the desired user behavior that they want that entity to do.
+
+    - Difference from TDD?
+      - BDD is designed to test an application's behavior from the end user's standpoint whereas TDD is focused on testing smaller pieces of functionality in isolation.
+
+      What do we use to do testing in Angular?
+      - Karma and Jasmine to make our test cases in Angular
+      - Karma = the task runner = used to run our test cases that are located in .spec files throughout the app. Usually it runs with we execute the 'ng test' command
+      - Jasmine = a testing framework that is used to create our test cases (aka syntax)
+*/
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
+//this is how you make a test case using 'describe'
+//the describe will start our test block and give into about what component we are testing
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
@@ -19,5 +37,10 @@ describe('AboutComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  //to make a test case, we would use the it keyword
+  it("should have h4 with class 'main-title' to be 'About DBZ API'", () => {
+    expect(component.document.querySelector(".main-title").innerHTML).toBeTruthy("About DBZ API");
   });
 });
--- demos/8-Angular/dbz-angular-demo/dbz-app/src/app/components/about/about.component.ts
+++ demos/8-Angular/dbz-angular-demo/dbz-app/src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) public document: Document) { }
 
   ngOnInit(): void {
   }
